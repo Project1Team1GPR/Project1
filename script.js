a77b878f4972427b19dfbe244eb1dc155e6757e5
gmeyer24 <144490098+gmeyer24@users.noreply.github.com>
2023-11-07 21:36:21 -0600

    set up activity api

---
+++ script.js
@@ -0,0 +1,45 @@
+var activity = document.getElementById("activity");
+var url = `https://api.api-ninjas.com/v1/caloriesburned?activity=${activityName}&duration=60&weight=200`;
+
+function searchActivity() {
+  fetch(url, {
+    headers: {
+      "X-Api-Key": "Wjicx6SkiBem7pplQibm7g==wVPkDcY9lX6RAcn0",
+    },
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      activity.textContent = data;
+
+      if (!data.length) {
+        console.log("No results found");
+        activity.textContent = "No Results Found";
+      } else {
+        activity.textContent = "";
+        for (let i = 0; i < data.length; i++) {
+
+          var activityName = data[i].name;
+          var activityEl = document.createElement("h3");
+          activityEl.textContent = `Activity: ${activityName}`;
+          activity.appendChild(activityEl)
+
+          var caloriesBurned = data[i].total_calories;
+          var calorieEl = document.createElement("p");
+          calorieEl.textContent = `Calories Burned: ${caloriesBurned}`;
+          activity.appendChild(calorieEl);
+
+          var activityDuration = data[i].duration_minutes;
+          var durationEl = document.createElement("p");
+          durationEl.textContent = `Activity Duration: ${activityDuration} min`;
+          activity.appendChild(durationEl);
+          
+
+        }
+      }
+    });
+}
+
+searchActivity();
