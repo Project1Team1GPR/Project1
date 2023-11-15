82bdb62f183d1f581ab393e5c41d543ab39090b4
gmeyer24 <144490098+gmeyer24@users.noreply.github.com>
2023-11-14 22:35:37 -0600

    made changes to mvp: js html and css
    Co-authored-by: Patric Xiong <PatricXiong@users.noreply.github.com>
    Co-authored-by: 1122c <1122c@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 var activity = document.getElementById("activity");
-var userActivityInput = document.getElementById("userActivity");
+// var userActivityInput = document.getElementById("userActivity");
 var submitButtonEl = document.getElementById("submitButton");
 var userActivitySelect = document.getElementById("activitySelect");
-var submitActivityButtonEl = document.getElementById("activitySubmitButton");
+// var submitActivityButtonEl = document.getElementById("activitySubmitButton");
 var submitRecipeButtonEl = document.getElementById("recipeSubmitButton");
 var userWeightInput = document.getElementById("weight");
 // var durationEl = document.getElementById("duration");
 var resultEl = document.getElementById("result");
-var calories = 1000;
+// var calories = 1000;
 var recipeSelect = document.getElementById("recipeSelect");
+var caloriesPerMinute = 1 
+var userSelectedActivity = "";
 
 // step 1- user selects recipe, user gets # of calories needed to be burned - api spoon
 // Step 2- user selects activity type from drop down list
@@ -21,8 +23,56 @@ var recipeSelect = document.getElementById("recipeSelect");
 // 3. Select recipe
 // 4. take total cals from selected recipe to help determine total duration of selected activity
 
-// delete
-// var recipeIdTest = "716429"; //need to find correct ID
+
+
+function activityList() {
+  var exerciseListUrl = `https://api.api-ninjas.com/v1/caloriesburnedactivities`;
+
+  fetch(exerciseListUrl, {
+    headers: {
+      "X-Api-Key": "Wjicx6SkiBem7pplQibm7g==wVPkDcY9lX6RAcn0",
+    },
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      data.activities.unshift("--Pick an Activity--");
+      data.activities.forEach(function (activity) {
+        var option = document.createElement("option");
+        option.value = activity;
+        option.text = activity;
+        userActivitySelect.appendChild(option);
+      });
+    });
+}
+activityList();
+
+userActivitySelect.addEventListener("change", function(event){
+  event.preventDefault();
+  console.log(event.target.value);
+
+  var caloriesBurnedUrl = `https://api.api-ninjas.com/v1/caloriesburned?activity=${event.target.value.substring(1)}&duration=60&weight=200`;
+
+  // var userWeight = userWeightInput.value;
+  
+  fetch(caloriesBurnedUrl, {
+    headers: {
+      "X-Api-Key": "Wjicx6SkiBem7pplQibm7g==wVPkDcY9lX6RAcn0",
+    },
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      // calculate calories per minute
+      caloriesPerMinute = data[0].calories_per_hour / 60;
+      userSelectedActivity = userActivitySelect.value;
+      console.log(userSelectedActivity);
+    });
+});
 
 async function searchRecipesByQuery(query) {
   var apiKey = "63c92a06cbdb4547b9f28e0fcbc3c5c3";
@@ -62,9 +112,15 @@ function appendRecipeResults(recipeResultsEl, recipes) {
     <div><b>${recipe.title}</b></div>
       <img src="${recipe.image}">
       <p>Total Calories: ${recipe.calories}</p>
+      <p>It will take ${Math.round(recipe.calories / caloriesPerMinute)} minutes of ${userActivitySelect.value} to burn of this meal</p>
       <button data-id="${recipe.id}">Ingrediens</button>
       <div data-ingr="${recipe.id}" id="recipeIngredientList"></div>
       `;
+
+      // DELETE
+      // <p>Calories Burned per Minute for Selected Activity: ${caloriesPerMinute ? caloriesPerMinute.toFixed(2) : 'Select an activity'}</p>
+
+      
 
     recipeResultsEl.appendChild(recipeEl);
     console.log(recipe);
@@ -87,6 +143,7 @@ function displayIngredientsList(ingredients, recipeId) {
 // event listener for ingredients
 // find a way to consolidate this function & getCaloriesByRecipeId!! CLEAN UP
 $("#recipeResults").on("click", "button", async function (event) {
+  event.preventDefault();
   var recipeId = $(event.target).data("id");
   console.log(recipeId);
   var ingredients = await getIngredientsById(recipeId);
@@ -108,7 +165,7 @@ async function getIngredientsById(id) {
 }
 
 // submit recipe button on click
-submitRecipeButtonEl.addEventListener("click", function (event) {
+submitRecipeButtonEl.addEventListener("click", async function (event) {
   event.preventDefault();
   console.log("Submitted Recipe");
 
@@ -121,6 +178,10 @@ submitRecipeButtonEl.addEventListener("click", function (event) {
   });
 });
 
+  
+
+
+// DELETE BELOW
 // attempting to get calories displayed on screen
 // async function getCaloriesByRecipeId(id) {
 //   var nutritionUrl = `https://api.spoonacular.com/recipes/${id}/nutritionWidget.json?apiKey=63c92a06cbdb4547b9f28e0fcbc3c5c3`;
@@ -135,137 +196,140 @@ submitRecipeButtonEl.addEventListener("click", function (event) {
 //   }
 // }
 
-function activityList() {
-  var listUrl = `https://api.api-ninjas.com/v1/caloriesburnedactivities`;
 
-  fetch(listUrl, {
-    headers: {
-      "X-Api-Key": "Wjicx6SkiBem7pplQibm7g==wVPkDcY9lX6RAcn0",
-    },
-  })
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      data.activities.forEach(function (activity) {
-        var option = document.createElement("option");
-        option.value = activity;
-        option.text = activity;
-        userActivitySelect.appendChild(option);
-      });
-    });
-}
+// const activitySelect = document.getElementById("activitySelect");
+// const caloriesDisplay = document.getElementById("caloriesDisplay");
 
-activityList();
+// Function to fetch and display calories burned per hour for the selected activity
+// function displayCaloriesBurned(event) {
+//   const selectedActivity = userActivitySelect.value;
 
-var selectedActivity = userActivitySelect.value;
-var userWeight = userWeightInput.value;
+//   if (selectedActivity) {
+//     const url = `https://api.api-ninjas.com/v1/caloriesburned?activity=${selectedActivity}&duration=60&weight=200`; // Assuming a default weight of 200 lbs
 
-function allInputs() {
-  console.log("allInputs Works");
-  // get recipe cals from api call
+//     fetch(url, {
+//       headers: {
+//         "X-Api-Key": "Wjicx6SkiBem7pplQibm7g==wVPkDcY9lX6RAcn0",
+//       },
+//     })
+//       .then((response) => response.json())
+//       .then((data) => {
+//         const caloriesPerHour = data[0]?.calories_per_hour || 0;
+//         caloriesDisplay.textContent = `Calories Burned per Hour: ${caloriesPerHour}`;
+//       })
+//       .catch((error) => {
+//         console.error("Error fetching calories burned per hour:", error);
+//         caloriesDisplay.textContent = "Failed to fetch data";
+//       });
+//   } else {
+//     caloriesDisplay.textContent = "Please select an activity";
+//   }
+// }
 
-  // api call to get cals burned per min for the selected activity
+// Event listener for activity selection
+// userActivitySelect.addEventListener("change", displayCaloriesBurned);
 
-  var url = `https://api.api-ninjas.com/v1/caloriesburned?activity=${activity}&duration=60&weight=${userWeight}`;
-  fetch(url, {
-    headers: {
-      "X-Api-Key": "Wjicx6SkiBem7pplQibm7g==wVPkDcY9lX6RAcn0",
-    },
-  })
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
+// var selectedActivity = userActivitySelect.value;
+// var userWeight = userWeightInput.value;
 
-      if (!data.length) {
-        console.log("No results found");
-        resultEl.textContent = "No Results Found";
-      } else {
-        resultEl.textContent = "";
+// function allInputs() {
+//   console.log("allInputs Works");
+//   // get recipe cals from api call
 
-        for (let i = 0; i < data.length; i++) {
-          var activityName = data[i].activities;
-          var activityEl = document.createElement("h3");
-          activityEl.textContent = `Activity: ${activityName}`;
-          resultEl.appendChild(activityEl);
+//   // api call to get cals burned per min for the selected activity
 
-          var caloriesBurned = data[i].calories_per_hour;
-          var calorieEl = document.createElement("p");
-          calorieEl.textContent = `Calories Burned: ${caloriesBurned}`;
-          resultEl.appendChild(calorieEl);
-        }
-      }
+//   var url = `https://api.api-ninjas.com/v1/caloriesburned?activity=${activity}&duration=60&weight=${userWeight}`;
+//   // var selectedActivity = userActivitySelect.value;
+//   // var userWeight = userWeightInput.value;
+  
+//   fetch(url, {
+//     headers: {
+//       "X-Api-Key": "Wjicx6SkiBem7pplQibm7g==wVPkDcY9lX6RAcn0",
+//     },
+//   })
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
 
-      //   .catch(function (error) {
-      //     console.error('error:', error);
-      //     activityEl.textContent = 'cannot retrieve data';
+//       if (!data.length) {
+//         console.log("No results found");
+//         resultEl.textContent = "No Results Found";
+//       } else {
+//         resultEl.textContent = "";
 
-      // })
-    });
+//         for (let i = 0; i < data.length; i++) {
+//           var activityName = data[i].activities;
+//           var activityEl = document.createElement("h3");
+//           activityEl.textContent = `Activity: ${activityName}`;
+//           resultEl.appendChild(activityEl);
 
-  // then users weight
-}
+//           var caloriesBurned = data[i].calories_per_hour;
+//           var calorieEl = document.createElement("p");
+//           calorieEl.textContent = `Calories Burned: ${caloriesBurned}`;
+//           resultEl.appendChild(calorieEl);
+//         }
+//       }
 
-allInputs(calories, selectedActivity, userWeight);
+//       //   .catch(function (error) {
+//       //     console.error('error:', error);
+//       //     activityEl.textContent = 'cannot retrieve data';
 
-function calcActivityDuration(calsBurnedPerMin, calories = 1000) {
-  return calories / calsBurnedPerMin;
-}
+//       // })
+//     });
 
-function searchActivityData(userInput) {
-  var url = `https://api.api-ninjas.com/v1/caloriesburned?activity=ski&duration=60&weight=200`;
-  fetch(url, {
-    headers: {
-      "X-Api-Key": "Wjicx6SkiBem7pplQibm7g==wVPkDcY9lX6RAcn0",
-    },
-  })
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      activity.textContent = data;
+//   // then users weight
+// }
 
-      if (!data.length) {
-        console.log("No results found");
-        activity.textContent = "No Results Found";
-      } else {
-        activity.textContent = "";
-        for (let i = 0; i < data.length; i++) {
-          var activityName = data[i].name;
-          var activityEl = document.createElement("h3");
-          activityEl.textContent = `Activity: ${activityName}`;
-          activity.appendChild(activityEl);
+// allInputs(calories, selectedActivity, userWeight);
 
-          var caloriesBurned = data[i].total_calories;
-          var calorieEl = document.createElement("p");
-          calorieEl.textContent = `Calories Burned: ${caloriesBurned}`;
-          activity.appendChild(calorieEl);
+// function calcActivityDuration(calsBurnedPerMin, calories = 1000) {
+//   return calories / calsBurnedPerMin;
+// }
 
-          var activityDuration = data[i].duration_minutes;
-          var durationEl = document.createElement("p");
-          durationEl.textContent = `Activity Duration: ${activityDuration} min`;
-          activity.appendChild(durationEl);
-        }
-      }
-    });
-}
+// function searchActivityData(userInput) {
+//   var url = `https://api.api-ninjas.com/v1/caloriesburned?activity=ski&duration=60&weight=200`;
+//   fetch(url, {
+//     headers: {
+//       "X-Api-Key": "Wjicx6SkiBem7pplQibm7g==wVPkDcY9lX6RAcn0",
+//     },
+//   })
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       activity.textContent = data;
 
-// submit button on click
-// submitButtonEl.addEventListener("click", function (event) {
-//   event.preventDefault();
-//   console.log("Clicked");
-//   // userActivityInput.value;
-//   var userInput = userActivityInput.value;
-//   searchActivityData(userInput);
-// });
+//       if (!data.length) {
+//         console.log("No results found");
+//         activity.textContent = "No Results Found";
+//       } else {
+//         activity.textContent = "";
+//         for (let i = 0; i < data.length; i++) {
+//           var activityName = data[i].name;
+//           var activityEl = document.createElement("h3");
+//           activityEl.textContent = `Activity: ${activityName}`;
+//           activity.appendChild(activityEl);
+
+//           var caloriesBurned = data[i].total_calories;
+//           var calorieEl = document.createElement("p");
+//           calorieEl.textContent = `Calories Burned: ${caloriesBurned}`;
+//           activity.appendChild(calorieEl);
+
+//           var activityDuration = data[i].duration_minutes;
+//           var durationEl = document.createElement("p");
+//           durationEl.textContent = `Activity Duration: ${activityDuration} min`;
+//           activity.appendChild(durationEl);
+//         }
+//       }
+//     });
+// }
 
 // submit activity button on click
-submitActivityButtonEl.addEventListener("click", function (event) {
-  event.preventDefault();
-  console.log("Submit Activity");
-  allInputs();
-});
+// submitActivityButtonEl.addEventListener("click", function (event) {
+//   event.preventDefault();
+//   console.log("Submit Activity");
+//   allInputs();
+// });
